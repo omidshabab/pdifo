0abb73487733e9034353ee97ac0ddcfa073ae6d5
Omid Shabab <omidshababdev@gmail.com>
2024-04-22 13:13:10 -0400

    0.1.0

---
+++ src/components/elements/TextElement.tsx
@@ -0,0 +1,11 @@
+import Element from "../Element";
+
+const TextWidget = () => {
+     return (
+          <Element
+               id="text"
+               text="Text" />
+     );
+}
+
+export default TextWidget;
--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,2 +1,2 @@
-export const widgets: Array<Number> = [1, 2, 3, 4, 5, 6];
+export const elements: Array<Number> = [1, 2, 3, 4, 5, 6];
 export const pages: Array<Number> = [1, 2, 3, 4, 5, 6];
